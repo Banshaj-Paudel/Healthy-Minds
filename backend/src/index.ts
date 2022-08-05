5e35efc993b40a7c4520fcbc4330fc96e4dc2074
Neeraj Lamsal <itskop520@gmail.com>
2022-08-05 16:01:32 +0545

    [added] image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import { image_upload } from "./upload";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
-//app.use(morgan("dev"));
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use(fileUpload());
+app.use("/image", image_upload);
 
 app.get("/", (req, res) => {
   res.json("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port http://localhost:3000");
 });
