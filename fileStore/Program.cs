9dc85bd5c150d7186096bd283cb7e5a5fd03b696
Ranjan Ojha <59282692+Hungerarray@users.noreply.github.com>
2022-08-06 18:54:16 +0545

    Add API endpoints

--- fileStore/Program.cs
+++ fileStore/Program.cs
@@ -1,6 +1,18 @@
+using Ipfs.Engine;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var config = builder.Configuration;
+
+services.AddSingleton<IpfsEngine>( 
+  _ => new IpfsEngine(config["ipfs:passphrase"].ToCharArray())
+);
+services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.MapControllers();
 
 app.Run();
