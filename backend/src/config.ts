2031711071add0d0c8f93a72c8bf36886eb318fe
Samrid Pandit <64601348+CaffeineDuck@users.noreply.github.com>
2022-08-06 17:58:08 +0545

    add patient models and endpoints (#8)
    * feat: add CRUD for patient data
    
    * add tools version for asdf

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,3 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET!;
+export const API_URL = process.env.API_URL!;
---
+++ backend/src/utils/blockchainFile.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { API_URL } from "../config";
+
+export const getB64Bytes = async (fileName: string): Promise<string> => {
+  return "bytes";
+};
+
+export const uploadB64Bytes = async (b64Bytes: string): Promise<string> => {
+  return "filename.jpg";
+};
