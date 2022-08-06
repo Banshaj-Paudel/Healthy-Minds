6773b5931c10f2d8666981653427e4f5c9f21314
Mahesh C. Regmi <0xmahadev@gmail.com>
2022-08-06 22:31:43 +0545

    add breadcrumb and separate page for add patient

---
+++ dashboard/src/screens/AddPatientScreen.tsx
@@ -0,0 +1,12 @@
+import AddPatientData from "../components/AddPatientData";
+
+export const AddPatientScreen = () => {
+  return (
+    <div>
+      <div className="header">
+        <h2>Dashboard / Add Patient</h2>
+      </div>
+      <AddPatientData />
+    </div>
+  );
+};
